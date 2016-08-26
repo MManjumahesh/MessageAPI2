97e6051dd49b380a3bc99648b48d4a7f57596c77
MManjumahesh <manjumaheshwar@gmail.com>
2016-08-26 13:17:56 +0200

    Commit

--- MessageAPI2/src/main/java/com/cognizone/tests/manju/messageapi2/MessageApi2Application.java
+++ MessageAPI2/src/main/java/com/cognizone/tests/manju/messageapi2/MessageApi2Application.java
@@ -1,4 +1,4 @@
-package com.cognizone.tests.manju.messageapi2;
+ package com.cognizone.tests.manju.messageapi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
